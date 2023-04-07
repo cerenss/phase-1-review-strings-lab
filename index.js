5023a2922e0f040e726ef68a6c80ccdac6f79292
Ceren Şahin <cerennssahinn@gmail.com>
2023-04-07 19:19:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser = "Grace Hopper";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+// console.log(welcomeMessage, excitedWelcomeMessage, shortGreeting);
